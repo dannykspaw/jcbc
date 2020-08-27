00a0486b4daab0ff420b556917c6c92b8041e464
dannykspaw <dannykspaw@gmail.com>
2020-08-27 09:37:41 -0500

    Updated basic web_scraping file with hallmark ornaments just to see html complexity

--- web_scraping.py
+++ web_scraping.py
@@ -8,6 +8,6 @@ options.headless = True
 options.add_argument("--window-size=1920,1200")
 
 driver = webdriver.Chrome(options=options, executable_path=DRIVER_PATH)
-driver.get("https://www.nintendo.com/")
+driver.get("https://www.hallmarkornaments.com/")
 print(driver.page_source)
 driver.quit()
