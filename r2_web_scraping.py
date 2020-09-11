2eea097169671e68a1ff807d37041863a00bb2c1
dannykspaw <dannykspaw@gmail.com>
2020-09-11 16:36:53 -0500

    Pushing revised scrape

--- r2_web_scraping.py
+++ r2_web_scraping.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import numpy as np
+import pandas as pd
 import time
 
 # Establish webdriver path
@@ -39,23 +40,58 @@ for x in all_years:
 # print(year_link_list)
 
 #Find products in each year, this is a simple iteration that pulls the year links and product names
-for x in range(1973,1999):
+
+product_names = []
+product_links = []
+product_prices = []
+product_codes = []
+
+for x in range(1973,1978):
     driver.get(year_link_list[x])
     time.sleep(1)
     try:
         number_of_products = driver.find_element_by_class_name('product-count')
         number_of_products = number_of_products.text[26:]
-        #Working on viewing all products here
+        #Working on viewing all products here.text
         print("NUMBER OF PRODUCTS: ", number_of_products)
         time.sleep(1)
         product_name_list = driver.find_elements_by_class_name('name')
-        product_names = []
         for x in product_name_list:
-            print(x.text," : ", x.get_attribute("href"))
+            product_names.append(x.text)
+            product_link_location = driver.find_element_by_partial_link_text(x.text)
+            product_link = product_link_location.get_attribute("href")
+            product_links.append(product_link)
+            print(x.text, " : ", product_link)
+        print(len(product_links))
+        for x in product_links:
+            driver.get(x)
+            price = "price_placeholder"
+            price = driver.find_element_by_id("price").text
+            product_prices.append(price)
+            code = "code_placeholder"
+            # code = driver.find_element_by_id("product_id").text
+            product_codes.append(code)
         # iter_products_num = int(number_of_products)
         # for product in iter_products_num:
         #     product_name = driver.find_elements_by_class_name("name")
     except:
         print("NUMBER OF PRODUCTS: 0")
+
+print(product_codes)
+print(len(product_codes))
+print(product_prices)
+print(len(product_prices))
+print(product_names)
+print(len(product_names))
+print(product_links)
+print(len(product_links))
+
+# ornament_db = pd.DataFrame()
+# ornament_db.insert(0,"Product Name",product_names)
+# ornament_db.insert(1,"Product Code",product_codes)
+# ornament_db.insert(2,"Product Prices",product_prices)
+# ornament_db.insert(3,"Product Link",product_links)
+# ornament_db.head()
+
 print("DONE")
 driver.quit()
